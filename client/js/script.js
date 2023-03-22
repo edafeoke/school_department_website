ba3f708b766c0d547dcc8cdaa193a89bf829da5d
Edafe Oke Great <greatedafeoke@gmail.com>
2023-03-22 01:12:25 +0100

    Fixed index.html header glitches

--- client/js/script.js
+++ client/js/script.js
@@ -8,7 +8,9 @@ toggleButton.addEventListener("click", () => {
   dropDownMenu.classList.toggle("open");
   isOpen = dropDownMenu.classList.contains("open");
 
-  toggleButton.innerHTML = isOpen ? '<i class="fa-solid fa-xmark"></i>' : '<i class="fa-solid fa-bars"></i>';
+  toggleButton.innerHTML = isOpen
+    ? '<i class="fa-solid fa-xmark"></i>'
+    : '<i class="fa-solid fa-bars"></i>';
 });
 
 // document.querySelector('.content').addEventListener('click',()=>{
@@ -16,5 +18,28 @@ toggleButton.addEventListener("click", () => {
 //     dropDownMenu.classList.toggle("open");
 //   }
 
-
 // })
+
+//Header Animation
+const header = document.querySelector("header");
+
+let i = 1;
+
+const slideShow = () => {
+  setInterval(() => {
+    if (i == 1) {
+      header.classList.remove("header-back-3");
+      header.classList.add("header-back-1");
+      i = 2;
+    } else if (i == 2) {
+      header.classList.remove("header-back-1");
+      header.classList.add("header-back-2");
+      i = 3;
+    } else {
+      header.classList.remove("header-back-2");
+      header.classList.add("header-back-3");
+      i = 1;
+    }
+  }, 5000);
+};
+document.onload = slideShow();
